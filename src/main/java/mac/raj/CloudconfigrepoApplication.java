4e4847a01173c3dfe81e50ad6fb0fda4db9b87b3
deloveper <Panja@786>
2021-05-29 11:04:50 +0530

    first commit

---
+++ src/main/java/mac/raj/CloudconfigrepoApplication.java
@@ -0,0 +1,13 @@
+package mac.raj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudconfigrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudconfigrepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mac/raj/CloudconfigrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package mac.raj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudconfigrepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
